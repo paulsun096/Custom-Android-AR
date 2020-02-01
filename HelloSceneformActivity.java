5064d3c0033986a331bb1acce546cd98e1d04c3d
Paul Sun <paulsun096@gmail.com>
2020-02-01 18:18:38 -0500

    changed .sfb to generic name

--- HelloSceneformActivity.java
+++ HelloSceneformActivity.java
@@ -62,7 +62,7 @@ public class HelloSceneformActivity extends AppCompatActivity {
     // When you build a Renderable, Sceneform loads its resources in the background while returning
     // a CompletableFuture. Call thenAccept(), handle(), or check isDone() before calling get().
     ModelRenderable.builder()
-        .setSource(this, Uri.parse("taka.sfb")) //(this, R.raw.andy)
+        .setSource(this, Uri.parse("custom.sfb")) //(this, R.raw.andy)
         .build()
         .thenAccept(renderable -> andyRenderable = renderable)
         .exceptionally(
